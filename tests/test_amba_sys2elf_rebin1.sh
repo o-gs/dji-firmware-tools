ca6a9ef7e99c7060ed3834cde1fc624101972853
mefistotelis <mefistotelis@gmail.com>
2016-10-03 00:01:24 +0200

    amba_sys2elf: Added a unit test file for the script.
    The test converts a file to elf, then generates bin,
    and checks whether the result is exactly same as input.

---
+++ tests/test_amba_sys2elf_rebin1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BINFILE="P3X_FW_V01.08.0080_12_part_sys.a9s"
+ELFFILE="${BINFILE%.*}.elf"
+TESTFILE="${BINFILE%.*}-test.bin"
+
+echo '### TEST for amba_sys2elf.py re-creation of binary file ###'
+# The test converts BIN to ELF, and the converts the result back to BIN.
+# The test ends with success if the resulting BIN file is
+# exactly the same as input BIN file.
+
+# Remove files which will be created
+rm "${ELFFILE}" "${TESTFILE}"
+
+# Convert to ELF
+./amba_sys2elf.py -vvv -e -p "${BINFILE}" -o "${ELFFILE}"
+
+# write info about file
+#readelf -a amba_app_test.elf > amba_app_test.elf.txt
+
+# Convert back to final format
+/usr/bin/arm-none-eabi-objcopy -O binary "${ELFFILE}" "${TESTFILE}"
+
+# Compare converted with original
+cmp --silent "${BINFILE}" "${TESTFILE}"
+TEST_RESULT=$?
+
+# Cleanup
+rm "${ELFFILE}" "${TESTFILE}"
+
+if [ ${TEST_RESULT} == 0 ]; then
+  echo '### SUCCESS: File identical after conversion. ###'
+else
+  echo '### FAIL: File changed during conversion! ###'
+  exit 2
+fi
+
+exit 0
+
