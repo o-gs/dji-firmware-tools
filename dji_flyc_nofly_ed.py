4897a80c6c974b998be67b3873c85f1ae49c4233
mefistotelis <mefistotelis@gmail.com>
2017-02-12 12:19:35 +0100

    dji_flyc_nofly_ed: Prepared function to export zones to json file.
    Merged list of Airports ans Special Areas can now be exported
    to a format similar to one used in Dji Go. There is one difference,
    which is new "storage" property. This property informs whether the
    entry comes from airports list, special zones list or both.

--- dji_flyc_nofly_ed.py
+++ dji_flyc_nofly_ed.py
@@ -242,15 +242,7 @@ def flyc_nofly_cord_get(po, fwmdlfile, index):
   parprop['lng'] = enfcord.longitude/1000000.0
   return parprop
 
-def flyc_nofly_list(po, fwmdlfile):
-  """ Lists all flight controller no fly zones from firmware on screen table.
-  """
-  (po.nfzone_pos, po.nfzone_count) = flyc_nofly_zone_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
-  if po.nfzone_pos < 0:
-    raise ValueError("Flight controller no fly zones array signature not detected in input file.")
-  (po.nfcord_pos, po.nfcord_count) = flyc_nofly_cord_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
-  if po.nfcord_pos < 0:
-    raise ValueError("Flight controller no fly coords array signature not detected in input file.")
+def flyc_nofly_merged_zones_array(po, fwmdlfile):
   if (po.verbose > 0):
      print("{}: Merging No Fly arrays...".format(po.mdlfile))
   nfzones = []
@@ -270,7 +262,18 @@ def flyc_nofly_list(po, fwmdlfile):
        nfzones.append(parprop)
     parprop['storage'] |= NoFlyStorage.cord
     #print("{:5d} {:10.6f} {:11.6f}".format(i,parcord['lat'],parcord['lng']))
-  nfzones = sorted(nfzones, key=lambda k: -k['lat'])
+  return sorted(nfzones, key=lambda k: -k['lat'])
+
+def flyc_nofly_list(po, fwmdlfile):
+  """ Lists all flight controller no fly zones from firmware on screen table.
+  """
+  (po.nfzone_pos, po.nfzone_count) = flyc_nofly_zone_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
+  if po.nfzone_pos < 0:
+    raise ValueError("Flight controller no fly zones array signature not detected in input file.")
+  (po.nfcord_pos, po.nfcord_count) = flyc_nofly_cord_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
+  if po.nfcord_pos < 0:
+    raise ValueError("Flight controller no fly coords array signature not detected in input file.")
+  nfzones = flyc_nofly_merged_zones_array(po, fwmdlfile)
   for parprop in nfzones:
      print("{:5d} {:10.6f} {:11.6f} {:5d} {:4d} {:s}{:s} {:d} {:d} {:d}".format(parprop['area_id'],parprop['lat'],parprop['lng'],
        parprop['radius'],parprop['country'],"z" if (parprop['storage'] & NoFlyStorage.zone) != 0 else " ",
@@ -282,7 +285,42 @@ def flyc_nofly_extract(po, fwmdlfile):
   (po.nfzone_pos, po.nfzone_count) = flyc_nofly_zone_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
   if po.nfzone_pos < 0:
     raise ValueError("Flight controller no fly zones array signature not detected in input file.")
-  raise NotImplementedError('Not implemented.')
+  (po.nfcord_pos, po.nfcord_count) = flyc_nofly_cord_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
+  if po.nfcord_pos < 0:
+    raise ValueError("Flight controller no fly coords array signature not detected in input file.")
+  nfzones = flyc_nofly_merged_zones_array(po, fwmdlfile)
+  inffile = open(po.inffile, "w")
+  inffile.write("{\"release_limits\":[\n")
+  i = 0
+  for parprop in nfzones:
+    inffile.write("{")
+    for ppname in ('area_id','type','shape',):
+       inffile.write("\"{:s}\" : {:d}".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('lat','lng',):
+       inffile.write("\"{:s}\" : {:06f}".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('radius','warning','level','disable','updated_at','begin_at','end_at',):
+       inffile.write("\"{:s}\" : {:d}".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('name',):
+       inffile.write("\"{:s}\" : \"{:s}\"".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('storage','country',):
+       inffile.write("\"{:s}\" : {:d}".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('city',):
+       inffile.write("\"{:s}\" : \"{:s}\"".format(ppname,parprop[ppname]))
+       inffile.write(",")
+    for ppname in ('points',):
+       inffile.write("\"{:s}\" : {:s}".format(ppname,parprop[ppname] if parprop[ppname] is not None else "null"))
+    if (i+1 < len(nfzones)):
+       inffile.write("},\n")
+    else:
+       inffile.write("}\n")
+    i += 1
+  inffile.write("]}\n")
+  inffile.close()
 
 def flyc_nofly_update(po, fwmdlfile):
   """ Updates all flight controller no fly zones in firmware from JSON format text file.
@@ -290,6 +328,9 @@ def flyc_nofly_update(po, fwmdlfile):
   (po.nfzone_pos, po.nfzone_count) = flyc_nofly_zone_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
   if po.nfzone_pos < 0:
     raise ValueError("Flight controller no fly zones array signature not detected in input file.")
+  (po.nfcord_pos, po.nfcord_count) = flyc_nofly_cord_pos_search(po, fwmdlfile, 0, po.expect_func_align, po.expect_data_align, po.min_match_accepted)
+  if po.nfcord_pos < 0:
+    raise ValueError("Flight controller no fly coords array signature not detected in input file.")
   raise NotImplementedError('Not implemented.')
 
 def main(argv):
