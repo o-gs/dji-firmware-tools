c0f75bdd777aa6526d1d72de5a01df8fe240d60e
Freek van Tienen <freek.v.tienen@gmail.com>
2017-06-27 23:03:40 +0200

    dji_imah_fwsig: Add copyright notice

--- dji_imah_fwsig.py
+++ dji_imah_fwsig.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 #
+# Copyright (C) 2017  Freek van Tienen <freek.v.tienen@gmail.com>
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
