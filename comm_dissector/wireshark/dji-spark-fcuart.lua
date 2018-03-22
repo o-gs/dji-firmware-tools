f1e2780aa66566ffe6b50eff291bb03aefcb8e08
mefistotelis <mefistotelis@gmail.com>
2018-03-22 21:20:58 +0100

    comm_dissector: Added Set Region packet to Spark.
    The packet which switches CE/FCC mode is now known.

--- comm_dissector/wireshark/dji-spark-fcuart.lua
+++ comm_dissector/wireshark/dji-spark-fcuart.lua
@@ -88,6 +88,7 @@ local RC_CMDS = {
 }
 
 local WIFI_CMDS = {
+    [0x30] = 'Set Region',
 }
 
 local DM36X_CMDS = {
@@ -158,7 +159,32 @@ local CENTER_BRD_DISSECT = {
 local RC_DISSECT = {
 }
 
+-- Wi-Fi - Set Region - 0x30
+
+f.flyc_wifi_set_region_str1 = ProtoField.string ("dji_spark.flyc_wifi_set_region_str1", "Region Str1", base.NONE)
+f.flyc_wifi_set_region_str2 = ProtoField.string ("dji_spark.flyc_wifi_set_region_str2", "Region Str2", base.NONE)
+f.flyc_wifi_set_region_unkn8 = ProtoField.uint16 ("dji_spark.flyc_wifi_set_region_unkn8", "Unknown8", base.HEX)
+
+local function flyc_wifi_set_region_dissector(pkt_length, buffer, pinfo, subtree)
+    local offset = 11
+    local payload = buffer(offset, pkt_length - offset - 2)
+    offset = 0
+
+    subtree:add_le (f.flyc_wifi_set_region_str1, payload(offset, 4))
+    offset = offset + 4
+
+    subtree:add_le (f.flyc_wifi_set_region_str2, payload(offset, 4))
+    offset = offset + 4
+
+    subtree:add_le (f.flyc_wifi_set_region_unkn8, payload(offset, 2))
+    offset = offset + 2
+
+    if (offset ~= 10) then subtree:add_expert_info(PI_MALFORMED,PI_ERROR,"Set Region: Offset does not match - internal inconsistency") end
+    if (payload:len() ~= offset) then subtree:add_expert_info(PI_PROTOCOL,PI_WARN,"Set Region: Payload size different than expected") end
+end
+
 local WIFI_DISSECT = {
+    [0x30] = flyc_wifi_set_region_dissector,
 }
 
 local DM36X_DISSECT = {
