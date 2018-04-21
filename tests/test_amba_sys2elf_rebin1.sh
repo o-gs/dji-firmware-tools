bfc06562f806b2684c25cbdb32b334278ce38bcd
mefistotelis <mefistotelis@gmail.com>
2018-04-22 01:43:03 +0200

    license: Added GPL headers to more scripts.

--- tests/test_amba_sys2elf_rebin1.sh
+++ tests/test_amba_sys2elf_rebin1.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016,2017 Mefistotelis <mefistotelis@gmail.com>
+# Copyright (C) 2018 Original Gangsters <https://dji-rev.slack.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 SKIP_MKELF=0
 SKIP_REBIN=0
