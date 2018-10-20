f3adc3a08e06d2f0f84c275b230ee4e9479e5440
mefistotelis <mefistotelis@gmail.com>
2018-10-20 20:24:52 +0200

    dji_flyc_hardcoder: More of P3X support.
    More values extracted.

--- dji_flyc_hardcoder.py
+++ dji_flyc_hardcoder.py
@@ -324,6 +324,58 @@ loc_val_fail:
 }
 
 
+re_func_wp_check_input_mission_validity_chunk2_P3X_V01_05_0030 = {
+'name': "wp_check_input_mission_validity_chunk2",
+'version': "P3X_FW_V01.05.0030",
+'re': """
+wp_check_input_mission_validity_chunk2:
+  vmov	r0, s0
+  ldr	r1, \[pc, #(?P<max_wp_dist_to_home>[0-9a-fx]+)\]
+  cmp	r0, r1
+  ble	#(?P<loc_806423A>[0-9a-fx]+)
+  mov	r1, r5
+  adr	r0, #(?P<cstr_wp_dist_too_large>[0-9a-fx]+)
+  bl	#(?P<flight_rec_printf_send_c0E>[0-9a-fx]+)
+  movs	r0, #0xe6
+  b	#(?P<loc_806421C>[0-9a-fx]+)
+loc_806423A:
+  ldrb	r0, \[r7, #0xd\]
+  cmp	r0, #1
+  bne	#(?P<loc_8064248>[0-9a-fx]+)
+  ldrb	r0, \[r7\]
+  subs	r0, r0, #1
+  cmp	r0, r5
+  beq	#(?P<loc_8064298>[0-9a-fx]+)
+loc_8064248:
+  ldrb	r0, \[r7, #0xf\]
+  cmp	r0, #1
+  bne	#(?P<loc_8064298>[0-9a-fx]+)
+  vldr	s1, \[r4, #0x14\]
+  ldr	r1, \[pc, #(?P<flt_minus_twentytwo_dot_four>[0-9a-fx]+)\]
+  vmov	r0, s1
+  add	r0, r1
+  ldr	r1, \[pc, #(?P<flt_positive_epsylon>[0-9a-fx]+)\]
+  cmp	r0, r1
+  blo	#(?P<loc_806426A>[0-9a-fx]+)
+; The function continues
+""",
+'vars': {
+  #'wp_check_input_mission_validity_chunk2':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'flight_rec_printf_send_c0E':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.FUNCTION},
+  'loc_806423A':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_806421C':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_8064248':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_806426A':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_8064298':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'cstr_wp_dist_too_large':	{'type': VarType.RELATIVE_PC_ADDR_TO_PTR_TO_GLOBAL_DATA, 'variety': DataVariety.CHAR, 'array': "null_term"},
+  'flt_minus_twentytwo_dot_four':	{'type': VarType.RELATIVE_PC_ADDR_TO_GLOBAL_DATA, 'variety': DataVariety.FLOAT},
+  'flt_positive_epsylon':	{'type': VarType.RELATIVE_PC_ADDR_TO_GLOBAL_DATA, 'variety': DataVariety.FLOAT},
+  'max_wp_dist_to_home':	{'type': VarType.RELATIVE_PC_ADDR_TO_GLOBAL_DATA, 'variety': DataVariety.FLOAT,
+    'public': "og_hardcoded.flyc", 'minValue': "10.0", 'maxValue': "1000000.0", 'defaultValue': "2000.0",
+    'description': "Max distance from one waypoint to home point"},
+},
+}
+
 re_func_wp_check_input_mission_validity_chunk2_WM100_V03_02_34_02 = {
 'name': "wp_check_input_mission_validity_chunk2",
 'version': "wm100_0306_v03.02.34.02",
@@ -395,6 +447,50 @@ loc_4ADD16:
 },
 }
 
+re_func_wp_check_input_mission_validity_chunk3_P3X_V01_05_0030 = {
+'name': "wp_check_input_mission_validity_chunk3",
+'version': "P3X_FW_V01.05.0030",
+'re': """
+wp_check_input_mission_validity_chunk3:
+  adds	r5, r5, #1
+  uxtb	r5, r5
+loc_806429C:
+  ldrb	r0, \[r7\]
+  cmp	r0, r5
+  bhi.w	#(?P<loc_8064142>[0-9a-fx]+)
+  bl	#(?P<sub_8064078>[0-9a-fx]+)
+  ldr	r0, \[pc, #(?P<byte_200081F8>[0-9a-fx]+)\]
+  ldr	r1, \[pc, #(?P<max_mission_path_len>[0-9a-fx]+)\]
+  vstr	s0, \[r0, #0x2bc\]
+  vmov	r0, s0
+  cmp	r0, r1
+  ble	#(?P<loc_80642D2>[0-9a-fx]+)
+  vmov	r0, s0
+  bl	#(?P<calculate_cali_matrix>[0-9a-fx]+)
+  vmov	d0, r0, r1
+  ldr	r0, \[pc, #(?P<cstr_total_dis_too_long>[0-9a-fx]+)\]
+  vmov	r2, r3, d0
+  bl	#(?P<flight_rec_printf_send_c0E>[0-9a-fx]+)
+  movs	r0, #0xe2
+  b	#(?P<loc_806421C>[0-9a-fx]+)
+; The function continues
+""",
+'vars': {
+  #'wp_check_input_mission_validity_chunk3':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_8064142':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_80642D2':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'loc_806421C':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.CHUNK},
+  'flight_rec_printf_send_c0E':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.FUNCTION},
+  'calculate_cali_matrix':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.FUNCTION},
+  'sub_8064078':	{'type': VarType.ABSOLUTE_ADDR_TO_CODE, 'variety': CodeVariety.FUNCTION},
+  'cstr_total_dis_too_long':	{'type': VarType.RELATIVE_PC_ADDR_TO_PTR_TO_GLOBAL_DATA, 'variety': DataVariety.CHAR, 'array': "null_term"},
+  'byte_200081F8':	{'type': VarType.RELATIVE_PC_ADDR_TO_GLOBAL_DATA, 'variety': DataVariety.UNKNOWN},
+  'max_mission_path_len':	{'type': VarType.RELATIVE_PC_ADDR_TO_GLOBAL_DATA, 'variety': DataVariety.FLOAT,
+    'public': "og_hardcoded.flyc", 'minValue': "10.0", 'maxValue': "1000000.0", 'defaultValue': "30000.0",
+    'description': "Max total length of mission"},
+},
+}
+
 re_func_wp_check_input_mission_validity_chunk3_WM100_V03_02_34_02 = {
 'name': "wp_check_input_mission_validity_chunk3",
 'version': "wm100_0306_v03.02.34.02",
@@ -443,6 +539,7 @@ loc_4ADF06:
     'description': "Max total length of mission"},
 },
 }
+
 
 re_func_wp_mission_data_verify_chunk1_WM100_V03_02_34_02 = {
 'name': "wp_mission_data_verify_chunk1",
@@ -612,7 +709,9 @@ loc_4AE12C:
 re_general_list = [
   {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_P3X_V01_05_0030,},
   {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_WM100_V03_02_34_02,},
+  {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_chunk2_P3X_V01_05_0030,},
   {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_chunk2_WM100_V03_02_34_02,},
+  {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_chunk3_P3X_V01_05_0030,},
   {'sect': ".text", 'func': re_func_wp_check_input_mission_validity_chunk3_WM100_V03_02_34_02,},
   {'sect': ".text", 'func': re_func_wp_mission_data_verify_chunk1_WM100_V03_02_34_02,},
 ]
