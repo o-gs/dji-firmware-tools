312097f057498bf933e4cd59f69188bc124931a7
mefistotelis <mefistotelis@gmail.com>
2018-11-27 00:45:16 +0100

    tests: Updated limits to include version changes.
    Changing firmware version does a lot more modifications to the
    firmware data.

--- tests/test_hardcoders_wm330.sh
+++ tests/test_hardcoders_wm330.sh
@@ -87,7 +87,7 @@ function exec_mod_for_m0306 {
 
   # Verify by checking amount of changes within the file
   set +x
-  verify_changed_bytes_between_files 11 32 "${FWMODL}.orig.bin" "${FWMODL}.bin"
+  verify_changed_bytes_between_files 36 48 "${FWMODL}.orig.bin" "${FWMODL}.bin"
   echo "### SUCCESS: Binary file changes are within acceptable limits. ###"
 }
 
