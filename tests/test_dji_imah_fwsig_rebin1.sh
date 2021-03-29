8b10cbbbbd5efd26fc568f3d1a4e1eb6adafed7d
mefistotelis <mefistotelis@gmail.com>
2021-03-29 12:16:43 +0200

    tests: Output directories fix in IMaH unsigner tests

--- tests/test_dji_imah_fwsig_rebin1.sh
+++ tests/test_dji_imah_fwsig_rebin1.sh
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+set -eo pipefail
+
 SKIP_EXTRACT=0
 SKIP_REPACK=0
 SKIP_CLEANUP=0
@@ -63,8 +65,7 @@ if [ ! -f "${BINFILE}" ]; then
 fi
 
 TESTFILE="${BINFILE%.*}-test.sig"
-HAS_0305=
-HAS_0306=
+HAS_MVFC_ENC=
 
 if [ "${SKIP_COMPARE}" -le "0" ]; then
   echo '### TEST for dji_imah_fwsig.py and dji_mvfc_fwpak.py re-creation of binary file ###'
@@ -77,52 +78,45 @@ fi
 if [ "${SKIP_EXTRACT}" -le "0" ]; then
   echo "### INFO: Input file \"${BINFILE}\" ###"
   # Remove files which will be created
+  set +e
   rm ${TESTFILE%.*}_*.bin ${TESTFILE%.*}_*.ini 2>/dev/null
+  set -e
   # Unsign/decrypt the module
   ./dji_imah_fwsig.py -vv -u -i "${BINFILE}" -m "${TESTFILE%.*}"
   # Some modules have another stage of encryption
-  HAS_0305=$(grep '^modules=\([0-9]\{4\}[ ]\)*0305' "${TESTFILE%.*}_head.ini" | head -n 1)
-  HAS_0306=$(grep '^modules=\([0-9]\{4\}[ ]\)*0306' "${TESTFILE%.*}_head.ini" | head -n 1)
-  if [ ! -z "${HAS_0305}" ]; then
-    MODULE="0305"
+  HAS_MVFC_ENC=$(sed -n 's/^modules=\([0-9]\{4\}[ ]\)*\(0305\|0306\).*$/\2/p' "${TESTFILE%.*}_head.ini" | head -n 1)
+  if [ ! -z "${HAS_MVFC_ENC}" ]; then
+    MODULE="${HAS_MVFC_ENC}"
     echo "### INFO: Found m${MODULE} inside, doing 2nd stage decrypt ###"
-    ./dji_mvfc_fwpak.py dec -i "${TESTFILE%.*}_${MODULE}.bin" | tee "${TESTFILE%.*}_${MODULE}.log"
-  fi
-  if [ ! -z "${HAS_0306}" ]; then
-    MODULE="0306"
-    echo "### INFO: Found m${MODULE} inside, doing 2nd stage decrypt ###"
-    ./dji_mvfc_fwpak.py dec -i "${TESTFILE%.*}_${MODULE}.bin" | tee "${TESTFILE%.*}_${MODULE}.log"
+    ./dji_mvfc_fwpak.py dec -i "${TESTFILE%.*}_${MODULE}.bin" \
+      -o "${TESTFILE%.*}_${MODULE}.decrypted.bin" | tee "${TESTFILE%.*}_${MODULE}.log"
   fi
 fi
 
 if [ "${SKIP_REPACK}" -le "0" ]; then
   # Remove file which will be created
-  if [ ! -z "${HAS_0305}" ]; then
-    rm "${TESTFILE%.*}_0305.bin" 2>/dev/null
-  fi
-  if [ ! -z "${HAS_0306}" ]; then
-    rm "${TESTFILE%.*}_0306.bin" 2>/dev/null
+  set +e
+  if [ ! -z "${HAS_MVFC_ENC}" ]; then
+    rm "${TESTFILE%.*}_${HAS_MVFC_ENC}.bin" 2>/dev/null
   fi
   rm "${TESTFILE}" 2>/dev/null
+  set -e
   # We do not have private parts of auth keys used for signing - use OG community key instead
   # Different signature means we will get up to 256 different bytes in the resulting file
   # Additional 2 bytes of difference is the FourCC - two first bytes of it were changed
   sed -i "s/^auth_key=[0-9A-Za-z]\{4\}$/auth_key=SLAK/" "${TESTFILE%.*}_head.ini"
   # Encrypt and sign back to final format
-  if [ ! -z "${HAS_0305}" ]; then
-    MODULE="0305"
+  if [ ! -z "${HAS_MVFC_ENC}" ]; then
+    MODULE="${HAS_MVFC_ENC}"
     MOD_FWVER=$(sed    -n "s/^Version:[ \t]*\([0-9A-Za-z. :_-]*\)$/\1/p" "${TESTFILE%.*}_${MODULE}.log" | head -n 1)
-    MOD_TMSTAMP=$(sed  -n "s/^Time:[ \t]*\([0-9A-Za-z. :_-]*\)$/\1/p"     "${TESTFILE%.*}_${MODULE}.log" | head -n 1)
-    ./dji_mvfc_fwpak.py enc -V "${MOD_FWVER}" -T "${MOD_TMSTAMP}" -t "${MODULE}" -i "${TESTFILE%.*}_${MODULE}.decrypted.bin"
+    MOD_TMSTAMP=$(sed  -n "s/^Time:[ \t]*\([0-9A-Za-z. :_-]*\)$/\1/p"    "${TESTFILE%.*}_${MODULE}.log" | head -n 1)
+    ./dji_mvfc_fwpak.py enc -V "${MOD_FWVER}" -T "${MOD_TMSTAMP}" -t "${MODULE}" \
+      -i "${TESTFILE%.*}_${MODULE}.decrypted.bin" -o "${TESTFILE%.*}_${MODULE}.bin"
   fi
-  if [ ! -z "${HAS_0306}" ]; then
-    MODULE="0306"
-    MOD_FWVER=$(sed    -n "s/^Version:[ \t]*\([0-9A-Za-z. :_-]*\)$/\1/p" "${TESTFILE%.*}_${MODULE}.log" | head -n 1)
-    MOD_TMSTAMP=$(sed  -n "s/^Time:[ \t]*\([0-9A-Za-z. :_-]*\)$/\1/p"     "${TESTFILE%.*}_${MODULE}.log" | head -n 1)
-    ./dji_mvfc_fwpak.py enc -V "${MOD_FWVER}" -T "${MOD_TMSTAMP}" -t "${MODULE}" -i "${TESTFILE%.*}_${MODULE}.decrypted.bin"
-  fi
-  ./dji_imah_fwsig.py -vv -s -i "${TESTFILE}"
+  ./dji_imah_fwsig.py -vv -s -i "${TESTFILE}" -m "${TESTFILE%.*}"
 fi
+
+set +eo pipefail
 
 if [ "${SKIP_COMPARE}" -le "0" ]; then
   # Compare converted with original
