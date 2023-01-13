21c12275605c820f8f0c9deb58bf00941c02d82f
mefistotelis <mefistotelis@gmail.com>
2023-01-13 17:55:58 +0100

    tests: Added lightbridge_stm32_hardcoder test

---
+++ tests/test_lightbridge_stm32_hardcoder1.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+""" Test for dji-firmware-tools, lightbridge_stm32_hardcoder script.
+
+    This test verifies functions of the script by extracting the
+    hard-coded values from ELF, applying modifications, and re-applying
+    them, finally checking if the resulting files have expected
+    amount of changes.
+
+    This test requires modules to already be extracted from their
+    packagges, and specific modules used here to be converted to ELF.
+"""
+
+# Copyright (C) 2023 Mefistotelis <mefistotelis@gmail.com>
+# Copyright (C) 2023 Original Gangsters <https://dji-rev.slack.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import filecmp
+import glob
+import itertools
+import json
+import logging
+import os
+import re
+import shutil
+import sys
+import pathlib
+import pytest
+from unittest.mock import patch
+
+# Import the functions to be tested
+sys.path.insert(0, './')
+import filediff
+from lightbridge_stm32_hardcoder import main as lightbridge_stm32_hardcoder_main
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def case_lightbridge_stm32_hardcoder_ckmod(elf_inp_fn):
+    """ Test case for extraction and re-applying of hard-coded properties within ELFs.
+    """
+    LOGGER.info("Testcase file: {:s}".format(elf_inp_fn))
+    # Most files we are able to recreate with full accuracy
+    expect_json_changes = 99
+    expect_file_changes = [0,0]
+
+    # Special cases - ignoring differences for some specific files
+    # Unused - no special cases required, as of now
+    if (re.match(r'^.*C1_FW_V[0-9A-Z_.-]*_m1400[.]elf', elf_inp_fn, re.IGNORECASE)):
+        expect_json_changes = 8 # 1 x attenuation_override + 1 x attenuation_value + 6 x board_attenuation
+        expect_file_changes = [2+6, 2*3 + 3*4]
+    elif (re.match(r'^.*P3S_FW_V[0-9A-Z_.-]*_m1400[.]elf', elf_inp_fn, re.IGNORECASE) or
+      re.match(r'^.*P3X_FW_V[0-9A-Z_.-]*_m1400[.]elf', elf_inp_fn, re.IGNORECASE)):
+        expect_json_changes = 6 # 3 x authority_level + 3 x vid_setting_bitrates
+        expect_file_changes = [6, 6*4]
+    elif (re.match(r'^.*WM610_FW_V[0-9A-Z_.-]*_m1400[.]elf', elf_inp_fn, re.IGNORECASE)):
+        expect_json_changes = 2
+        expect_file_changes = [2, 2*4]
+
+    inp_path, inp_filename = os.path.split(elf_inp_fn)
+    inp_path = pathlib.Path(inp_path)
+    inp_basename, elf_fileext = os.path.splitext(inp_filename)
+    if len(inp_path.parts) > 1:
+        out_path = os.sep.join(["out"] + list(inp_path.parts[1:]))
+    else:
+        out_path = "out"
+    elf_out_fn = os.sep.join([out_path, "{:s}.mod.elf".format(inp_basename)])
+    json_ori_fn = os.sep.join([out_path, "{:s}.json".format(inp_basename)])
+    json_mod_fn = os.sep.join([out_path, "{:s}.mod.json".format(inp_basename)])
+
+    # Create json file with recognized hard-coded values
+    command = [os.path.join(".", "lightbridge_stm32_hardcoder.py"), "-vvv", "-x", "-e", elf_inp_fn, "-o", json_ori_fn]
+    LOGGER.info(' '.join(command))
+    with patch.object(sys, 'argv', command):
+        lightbridge_stm32_hardcoder_main()
+    # Modify the JSON
+    with open(json_ori_fn) as valfile:
+        params_list = json.load(valfile)
+    nchanges = 0
+    for par in params_list:
+        if re.match(r'^og_hardcoded[.]lightbridge_stm32[.]packet_received_attenuation_override$', par['name']):
+            par['setValue'] = 1
+            nchanges += 1
+            continue
+        if re.match(r'^og_hardcoded[.]lightbridge_stm32[.]packet_received_attenuation_value$', par['name']):
+            par['setValue'] = 0
+            nchanges += 1
+            continue
+        if re.match(r'^og_hardcoded[.]lightbridge_stm32[.]board_[a-z0-9]*_attenuation_[a-z0-9]*_[a-z0-9]*$', par['name']):
+            par['setValue'] = "0"
+            nchanges += 1
+            continue
+    with open(json_mod_fn, "w") as valfile:
+        valfile.write(json.dumps(params_list, indent=4))
+    assert nchanges >= expect_json_changes, "Performed too few JSON modifications ({:d}<{:d}): {:s}".format(nchanges, expect_json_changes, json_mod_fn)
+    # Make copy of the ELF file
+    shutil.copyfile(elf_inp_fn, elf_out_fn)
+    # Import json file back to elf
+    command = [os.path.join(".", "lightbridge_stm32_hardcoder.py"), "-vvv", "-u", "-o", json_mod_fn, "-e", elf_out_fn]
+    LOGGER.info(' '.join(command))
+    with patch.object(sys, 'argv', command):
+        lightbridge_stm32_hardcoder_main()
+
+    if True:
+        # Count byte differences between repackaged file and the original
+        nchanges =  filediff.diffcount(elf_inp_fn, elf_out_fn)
+        assert nchanges >= expect_file_changes[0], "Updated file differences below bounds ({:d}<{:d}): {:s}".format(nchanges, expect_file_changes[0], elf_inp_fn)
+        assert nchanges <= expect_file_changes[1], "Updated file differences above bounds ({:d}>{:d}): {:s}".format(nchanges, expect_file_changes[1], elf_inp_fn)
+    pass
+
+@pytest.mark.order(4) # must be run after test_amba_sys2elf_rebin
+@pytest.mark.parametrize("elf_inp_dir,test_nth", [
+    #('out/ag405-agras_mg_1s_octocopter',1,), # no matching modules
+    ('out/gl300abc-radio_control',3,),
+    ('out/gl300e-radio_control',1,),
+    ('out/hg910-ronin2_gimbal',1,),
+    ('out/p3c-phantom_3_std_quadcopter',1,),
+    ('out/p3s-phantom_3_adv_quadcopter',3,),
+    #('out/p3se-phantom_3_se_quadcopter',1,), # no patterns recognized by the hardcoder
+    ('out/p3x-phantom_3_pro_quadcopter',3,),
+    #('out/p3xw-phantom_3_4k_quadcopter',1,), # no patterns recognized by the hardcoder
+    ('out/wm610-t600_inspire_1_x3_quadcopter',3,),
+  ] )
+def test_lightbridge_stm32_hardcoder_ckmod(elf_inp_dir, test_nth):
+    """ Test extraction and re-applying of hard-coded properties within ELFs.
+    """
+    if test_nth < 1:
+        pytest.skip("limited scope")
+
+    elf_inp_filenames = [fn for fn in itertools.chain.from_iterable([ glob.glob(e) for e in (
+        "{}/*-split1/*_m1400.elf".format(elf_inp_dir),
+    #TODO add m0900, m1401
+    ) ]) if os.path.isfile(fn)]
+
+    # Remove unsupported files
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*MG1SRC_FW_V[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*LB2_GND_V[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*RC_[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*300E_MCU_[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*GL300E_V[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*GL300E_RC_V[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+    elf_inp_filenames = [fn for fn in elf_inp_filenames if not re.match(r'^.*P3C_FW_V[0-9A-Z_.-]*_m1400[.]elf', fn, re.IGNORECASE)]
+
+    if len(elf_inp_filenames) < 1:
+        pytest.skip("no files to test in this directory")
+
+    for elf_inp_fn in elf_inp_filenames[::test_nth]:
+        case_lightbridge_stm32_hardcoder_ckmod(elf_inp_fn)
+    pass
