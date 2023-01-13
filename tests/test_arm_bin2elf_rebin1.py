3cc78dea0061c6ecaa9c629cb9f2d503963ebcee
mefistotelis <mefistotelis@gmail.com>
2023-01-13 14:21:14 +0100

    tests: Test for arm_bin2elf

---
+++ tests/test_arm_bin2elf_rebin1.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+""" Test for dji-firmware-tools, arm_bin2elf script.
+
+    This test verifies functions of the script by generating elf
+    from the system binary, then stripping it back to plain binary,
+    and checking in the bin files are identical.
+
+    This test requires the plain binaries to already be extracted from their
+    packages.
+"""
+
+# Copyright (C) 2023 Mefistotelis <mefistotelis@gmail.com>
+# Copyright (C) 2023 Original Gangsters <https://dji-rev.slack.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import filecmp
+import glob
+import itertools
+import logging
+import os
+import re
+import subprocess
+import sys
+import pathlib
+import pytest
+from unittest.mock import patch
+
+# Import the functions to be tested
+sys.path.insert(0, './')
+from arm_bin2elf import main as arm_bin2elf_main
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def case_arm_bin2elf_rebin(modl_inp_fn):
+    """ Test case for ELF creation and stripping back to BIN files.
+    """
+    LOGGER.info("Testcase file: {:s}".format(modl_inp_fn))
+    # Most files we are able to recreate with full accuracy
+    file_specific_cmdargs = []
+    expect_file_changes = 0
+
+    # Special cases - setting certain params and error tolerance for specific files
+    if (re.match(r'^.*MG1SRC_FW_V[0-9A-Za-z_.-]*_m1400[.]bin', modl_inp_fn)):
+        file_specific_cmdargs = ["-l", "0x6000000", "--section", ".ARM.exidx@0x100D3A0:0"]
+    elif (modl_inp_fn.endswith("_m1400.bin")):
+        file_specific_cmdargs = ["-l", "0x6000000"]
+
+    inp_path, inp_filename = os.path.split(modl_inp_fn)
+    inp_path = pathlib.Path(inp_path)
+    inp_basename, modl_fileext = os.path.splitext(inp_filename)
+    if len(inp_path.parts) > 1:
+        out_path = os.sep.join(["out"] + list(inp_path.parts[1:]))
+    else:
+        out_path = "out"
+    elf_out_fn = os.sep.join([out_path, "{:s}.elf".format(inp_basename)]) # prefix for output files
+    modl_out_fn = os.sep.join([out_path, "{:s}.rebin{:s}".format(inp_basename, modl_fileext)])
+    # Extract the package
+    command = [os.path.join(".", "arm_bin2elf.py"), "-vv", "-e"] + file_specific_cmdargs + ["-p", modl_inp_fn, "-o", elf_out_fn]
+    LOGGER.info(' '.join(command))
+    with patch.object(sys, 'argv', command):
+        arm_bin2elf_main()
+    # Re-pack the package
+    command = ["arm-none-eabi-objcopy", "-O", "binary", elf_out_fn, modl_out_fn]
+    LOGGER.info(' '.join(command))
+    subprocess.run(command)
+    if expect_file_changes == 0:
+        # Compare repackaged file and the original byte-to-byte, do not count differences
+        match =  filecmp.cmp(modl_inp_fn, modl_out_fn, shallow=False)
+        assert match, "Re-stripped file different: {:s}".format(modl_inp_fn)
+    elif expect_file_changes == 999999:
+        # Check only if repackaged file size roughly matches the original
+        modl_inp_fsize = os.path.getsize(modl_inp_fn)
+        modl_out_fsize = os.path.getsize(modl_out_fn)
+        assert modl_out_fsize >= int(modl_inp_fsize * 0.95), "Re-stripped file too small: {:s}".format(modl_inp_fn)
+        assert modl_out_fsize <= int(modl_inp_fsize * 1.05), "Re-stripped file too large: {:s}".format(modl_inp_fn)
+    else:
+        assert 0, "Not implemented"
+    pass
+
+@pytest.mark.order(2) # must be run after test_dji_xv4_fwcon_rebin
+@pytest.mark.parametrize("modl_inp_dir,test_nth", [
+    ('out/a3-flight_controller',1,),
+    ('out/ag405-agras_mg_1s_octocopter',1,),
+    ('out/ai900_agr-a3_based_multicopter_platform',1,),
+    ('out/am603-n3_based_multicopter_platform',1,),
+    ('out/d_rtk-mobile_station',1,),
+    ('out/ennn-esc',1,),
+    ('out/gl300abc-radio_control',1,),
+    ('out/gl300e-radio_control',1,),
+    ('out/hg211-osmo_pocket_2',1,),
+    ('out/hg300-osmo_mobile',1,),
+    ('out/hg301-osmo_mobile_2',1,),
+    ('out/hg910-ronin2_gimbal',1,),
+    ('out/lbtx-lightbridge_2_video_tx',1,),
+    ('out/m100-matrice_100_quadcopter',1,),
+    ('out/m600-matrice_600_hexacopter',1,),
+    ('out/m600pro-matrice_600_pro_hexacopter',1,),
+    ('out/n3-flight_controller',1,),
+    ('out/osmo-osmo_x3_gimbal',1,),
+    ('out/osmo_action-sport_cam',1,),
+    ('out/osmo_fc350z-osmo_zoom_z3_gimbal',1,),
+    ('out/osmo_fc550-osmo_x5_gimbal',1,),
+    ('out/osmo_fc550r-osmo_x5raw_gimbal',1,),
+    ('out/ot110-osmo_pocket_gimbal',1,),
+    ('out/p3c-phantom_3_std_quadcopter',1,),
+    ('out/p3s-phantom_3_adv_quadcopter',1,),
+    ('out/p3se-phantom_3_se_quadcopter',1,),
+    ('out/p3x-phantom_3_pro_quadcopter',1,),
+    ('out/p3xw-phantom_3_4k_quadcopter',1,),
+    ('out/swr60g-matrice_600_swr_60g',1,),
+    ('out/wind-a3_based_multicopter_platform',1,),
+    ('out/wm610-t600_inspire_1_x3_quadcopter',1,),
+    ('out/wm610_fc350z-t600_inspire_1_z3_quadcopter',1,),
+    ('out/wm610_fc550-t600_inspire_1_pro_x5_quadcopter',1,),
+    ('out/zt300-datalink_pro',1,),
+  ] )
+def test_arm_bin2elf_rebin(modl_inp_dir, test_nth):
+    """ Test for ELF creation and stripping back to BIN files.
+    """
+    if test_nth < 1:
+        pytest.skip("limited scope")
+
+    modl_inp_filenames = [fn for fn in itertools.chain.from_iterable([ glob.glob(e) for e in (
+        "{}/*-split1/*_m1400.bin".format(modl_inp_dir),
+        "{}/*-split1/*_m1401.bin".format(modl_inp_dir),
+    ) ]) if os.path.isfile(fn)]
+
+    if len(modl_inp_filenames) < 1:
+        pytest.skip("no files to test in this directory")
+
+    for modl_inp_fn in modl_inp_filenames[::test_nth]:
+        case_arm_bin2elf_rebin(modl_inp_fn)
+    pass
