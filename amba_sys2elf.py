a343b5464e72833239d9f64c291a5acad6632df4
mefistotelis <mefistotelis@gmail.com>
2023-04-09 03:55:18 +0200

    amba_sys2elf: Unify style in main()

--- amba_sys2elf.py
+++ amba_sys2elf.py
@@ -89,7 +89,9 @@ import itertools
 sys.path.insert(0, './')
 from arm_bin2elf import eprint, armfw_bin2elf, parse_section_param
 
+
 BUFSIZE = 8*1024
+
 
 def syssw_read_base_address(po, fname):
     mem_addr = 0
@@ -146,57 +148,57 @@ def main():
     """
     # Parse command line options
     # Set optimal options for Ambarella A9 ARM firmware
-
     parser = argparse.ArgumentParser(description=__doc__.split('.')[0])
 
-    parser.add_argument("-p", "--fwpartfile", type=str, required=True,
-          help="Executable ARM firmware binary module file")
+    parser.add_argument('-p', '--fwpartfile', type=str, required=True,
+          help="executable ARM firmware binary module file")
 
-    parser.add_argument("-o", "--elffile", type=str,
+    parser.add_argument('-o', '--elffile', type=str,
           help="directory and file name of output ELF file " \
            "(default is base name of fwpartfile with extension switched to elf, in working dir)")
 
-    parser.add_argument("-t", "--tmpltfile", type=str, default="amba_sys2elf_template.elf",
-          help="Template ELF file to use header fields from (default is \"%(default)s\")")
+    parser.add_argument('-t', '--tmpltfile', type=str, default="amba_sys2elf_template.elf",
+          help="template ELF file to use header fields from (default is \"%(default)s\")")
 
     parser.add_argument('-l', '--addrspacelen', default=0x2000000, type=lambda x: int(x,0),
-          help="Set address space length after base; the tool will expect used " \
-            "addresses to end at baseaddr+addrspacelen, so it influences size " \
-            "of last section (defaults to 0x%(default)X)")
+          help=("set address space length after base; the tool will expect used "
+            "addresses to end at baseaddr+addrspacelen, so it influences size "
+            "of last section (defaults to 0x%(default)X)"))
 
-    parser.add_argument("-s", "--section", action='append', metavar='SECT@ADDR:LEN', type=parse_section_param,
-          help="Set section position and/or length; can be used to override " \
-           "detection of sections; setting section .ARM.exidx will influence " \
-           ".text and .data, moving them and sizing to fit one before and one " \
-           "after the .ARM.exidx. Parameters are: " \
-           "SECT - a text name of the section, as defined in elf template; multiple sections " \
-           "can be cloned from the same template section by adding index at end (ie. .bss2); " \
-           "ADDR - is an address of the section within memory (not input file position); " \
-           "LEN - is the length of the section (in both input file and memory, unless its " \
-           "uninitialized section, in which case it is memory size as file size is 0)")
+    parser.add_argument('-s', '--section', action='append', metavar="SECT@ADDR:LEN", type=parse_section_param,
+          help=("set section position and/or length; can be used to override "
+           "detection of sections; setting section .ARM.exidx will influence "
+           ".text and .data, moving them and sizing to fit one before and one "
+           "after the .ARM.exidx. Parameters are: "
+           "SECT - a text name of the section, as defined in elf template; multiple sections "
+           "can be cloned from the same template section by adding index at end (ie. .bss2); "
+           "ADDR - is an address of the section within memory (not input file position); "
+           "LEN - is the length of the section (in both input file and memory, unless its "
+           "uninitialized section, in which case it is memory size as file size is 0)"))
 
-    parser.add_argument("--dry-run", action="store_true",
-          help="Do not write any files or do permanent changes")
+    parser.add_argument('--dry-run', action='store_true',
+          help="do not write any files or do permanent changes")
 
-    parser.add_argument("-v", "--verbose", action="count", default=0,
-          help="Increases verbosity level; max level is set by -vvv")
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+          help="increases verbosity level; max level is set by -vvv")
 
     subparser = parser.add_mutually_exclusive_group()
 
-    subparser.add_argument("--inifile", type=str,
+    subparser.add_argument('--inifile', type=str,
           help="INI file name with base address (default is fwpartfile with a9h extension appended)")
 
     subparser.add_argument('-b', '--baseaddr', type=lambda x: int(x,0),
-          help='Set base address; first section will start at this memory location (default is to get address from INI file)')
+          help=("set base address; first section will start at this "
+            "memory location (default is to get address from INI file)"))
 
     subparser = parser.add_mutually_exclusive_group()
 
-    subparser.add_argument("-e", "--mkelf", action="store_true",
+    subparser.add_argument('-e', '--mkelf', action='store_true',
           help="make ELF file from a binary image")
 
-    subparser.add_argument("--version", action='version', version="%(prog)s {version} by {author}"
+    subparser.add_argument('--version', action='version', version="%(prog)s {version} by {author}"
             .format(version=__version__, author=__author__),
-          help="Display version information and exit")
+          help="display version information and exit")
 
     po = parser.parse_args()
 
@@ -209,8 +211,8 @@ def main():
     po.section_addr = {}
     po.section_size = {}
     for sect in po.section:
-        po.section_addr.update(sect["addr"])
-        po.section_size.update(sect["len"])
+        po.section_addr.update(sect['addr'])
+        po.section_size.update(sect['len'])
 
     po.basename = os.path.splitext(os.path.basename(po.fwpartfile))[0]
     # What differs Ambarella BIN from other BINs is INI file with base address inside
@@ -221,34 +223,31 @@ def main():
         po.elffile = po.basename + ".elf"
 
     if po.mkelf:
-
         if (po.verbose > 0):
-           print("{}: Opening for conversion to ELF".format(po.fwpartfile))
+            print("{}: Opening for conversion to ELF".format(po.fwpartfile))
         # read base address from INI file which should be there after AMBA extraction
         if po.baseaddr is None:
             po.baseaddr = syssw_read_base_address(po, po.inifile)
-        fwpartfile = open(po.fwpartfile, "rb")
-        if ".ARM.exidx" not in po.section_addr:
-            sectname = ".ARM.exidx"
-            bound_pos = amba_find_default_code_data_bound(po, fwpartfile)
-            if bound_pos is not None:
-                if (po.verbose > 0):
-                    print("{}: Found Ambarella-specific '{:s}' location".format(po.fwpartfile,sectname))
-                po.section_addr[sectname] = po.baseaddr + bound_pos
-                po.section_size[sectname] = 0
-            fwpartfile.seek(0, os.SEEK_SET)
-        armfw_bin2elf(po,fwpartfile)
-
-        fwpartfile.close();
+        with open(po.fwpartfile, 'rb') as fwpartfile:
+            if ".ARM.exidx" not in po.section_addr:
+                sectname = ".ARM.exidx"
+                bound_pos = amba_find_default_code_data_bound(po, fwpartfile)
+                if bound_pos is not None:
+                    if (po.verbose > 0):
+                        print("{}: Found Ambarella-specific '{:s}' location".format(po.fwpartfile, sectname))
+                    po.section_addr[sectname] = po.baseaddr + bound_pos
+                    po.section_size[sectname] = 0
+                fwpartfile.seek(0, os.SEEK_SET)
+            armfw_bin2elf(po, fwpartfile)
 
     else:
+        raise NotImplementedError("Unsupported command.")
 
-        raise NotImplementedError('Unsupported command.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         main()
     except Exception as ex:
         eprint("Error: "+str(ex))
-        #raise
+        if 0: raise
         sys.exit(10)
