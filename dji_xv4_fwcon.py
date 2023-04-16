cdef37d3e185b1799d9244a8bbb26eb84c30bea8
mefistotelis <mefistotelis@gmail.com>
2023-04-16 19:18:19 +0200

    dji_xv4_fwcon: Style fixes

--- dji_xv4_fwcon.py
+++ dji_xv4_fwcon.py
@@ -321,123 +321,154 @@ class FwPkgEntry(LittleEndianStructure):
 
 
 crc16_tab = [
-  0x0000, 0x1189, 0x2312, 0x329B, 0x4624, 0x57AD, 0x6536, 0x74BF, 0x8C48, 0x9DC1, 0xAF5A, 0xBED3, 0xCA6C, 0xDBE5, 0xE97E, 0xF8F7,
-  0x1081, 0x0108, 0x3393, 0x221A, 0x56A5, 0x472C, 0x75B7, 0x643E, 0x9CC9, 0x8D40, 0xBFDB, 0xAE52, 0xDAED, 0xCB64, 0xF9FF, 0xE876,
-  0x2102, 0x308B, 0x0210, 0x1399, 0x6726, 0x76AF, 0x4434, 0x55BD, 0xAD4A, 0xBCC3, 0x8E58, 0x9FD1, 0xEB6E, 0xFAE7, 0xC87C, 0xD9F5,
-  0x3183, 0x200A, 0x1291, 0x0318, 0x77A7, 0x662E, 0x54B5, 0x453C, 0xBDCB, 0xAC42, 0x9ED9, 0x8F50, 0xFBEF, 0xEA66, 0xD8FD, 0xC974,
-  0x4204, 0x538D, 0x6116, 0x709F, 0x0420, 0x15A9, 0x2732, 0x36BB, 0xCE4C, 0xDFC5, 0xED5E, 0xFCD7, 0x8868, 0x99E1, 0xAB7A, 0xBAF3,
-  0x5285, 0x430C, 0x7197, 0x601E, 0x14A1, 0x0528, 0x37B3, 0x263A, 0xDECD, 0xCF44, 0xFDDF, 0xEC56, 0x98E9, 0x8960, 0xBBFB, 0xAA72,
-  0x6306, 0x728F, 0x4014, 0x519D, 0x2522, 0x34AB, 0x0630, 0x17B9, 0xEF4E, 0xFEC7, 0xCC5C, 0xDDD5, 0xA96A, 0xB8E3, 0x8A78, 0x9BF1,
-  0x7387, 0x620E, 0x5095, 0x411C, 0x35A3, 0x242A, 0x16B1, 0x0738, 0xFFCF, 0xEE46, 0xDCDD, 0xCD54, 0xB9EB, 0xA862, 0x9AF9, 0x8B70,
-  0x8408, 0x9581, 0xA71A, 0xB693, 0xC22C, 0xD3A5, 0xE13E, 0xF0B7, 0x0840, 0x19C9, 0x2B52, 0x3ADB, 0x4E64, 0x5FED, 0x6D76, 0x7CFF,
-  0x9489, 0x8500, 0xB79B, 0xA612, 0xD2AD, 0xC324, 0xF1BF, 0xE036, 0x18C1, 0x0948, 0x3BD3, 0x2A5A, 0x5EE5, 0x4F6C, 0x7DF7, 0x6C7E,
-  0xA50A, 0xB483, 0x8618, 0x9791, 0xE32E, 0xF2A7, 0xC03C, 0xD1B5, 0x2942, 0x38CB, 0x0A50, 0x1BD9, 0x6F66, 0x7EEF, 0x4C74, 0x5DFD,
-  0xB58B, 0xA402, 0x9699, 0x8710, 0xF3AF, 0xE226, 0xD0BD, 0xC134, 0x39C3, 0x284A, 0x1AD1, 0x0B58, 0x7FE7, 0x6E6E, 0x5CF5, 0x4D7C,
-  0xC60C, 0xD785, 0xE51E, 0xF497, 0x8028, 0x91A1, 0xA33A, 0xB2B3, 0x4A44, 0x5BCD, 0x6956, 0x78DF, 0x0C60, 0x1DE9, 0x2F72, 0x3EFB,
-  0xD68D, 0xC704, 0xF59F, 0xE416, 0x90A9, 0x8120, 0xB3BB, 0xA232, 0x5AC5, 0x4B4C, 0x79D7, 0x685E, 0x1CE1, 0x0D68, 0x3FF3, 0x2E7A,
-  0xE70E, 0xF687, 0xC41C, 0xD595, 0xA12A, 0xB0A3, 0x8238, 0x93B1, 0x6B46, 0x7ACF, 0x4854, 0x59DD, 0x2D62, 0x3CEB, 0x0E70, 0x1FF9,
-  0xF78F, 0xE606, 0xD49D, 0xC514, 0xB1AB, 0xA022, 0x92B9, 0x8330, 0x7BC7, 0x6A4E, 0x58D5, 0x495C, 0x3DE3, 0x2C6A, 0x1EF1, 0x0F78
+  0x0000, 0x1189, 0x2312, 0x329B, 0x4624, 0x57AD, 0x6536, 0x74BF,
+  0x8C48, 0x9DC1, 0xAF5A, 0xBED3, 0xCA6C, 0xDBE5, 0xE97E, 0xF8F7,
+  0x1081, 0x0108, 0x3393, 0x221A, 0x56A5, 0x472C, 0x75B7, 0x643E,
+  0x9CC9, 0x8D40, 0xBFDB, 0xAE52, 0xDAED, 0xCB64, 0xF9FF, 0xE876,
+  0x2102, 0x308B, 0x0210, 0x1399, 0x6726, 0x76AF, 0x4434, 0x55BD,
+  0xAD4A, 0xBCC3, 0x8E58, 0x9FD1, 0xEB6E, 0xFAE7, 0xC87C, 0xD9F5,
+  0x3183, 0x200A, 0x1291, 0x0318, 0x77A7, 0x662E, 0x54B5, 0x453C,
+  0xBDCB, 0xAC42, 0x9ED9, 0x8F50, 0xFBEF, 0xEA66, 0xD8FD, 0xC974,
+  0x4204, 0x538D, 0x6116, 0x709F, 0x0420, 0x15A9, 0x2732, 0x36BB,
+  0xCE4C, 0xDFC5, 0xED5E, 0xFCD7, 0x8868, 0x99E1, 0xAB7A, 0xBAF3,
+  0x5285, 0x430C, 0x7197, 0x601E, 0x14A1, 0x0528, 0x37B3, 0x263A,
+  0xDECD, 0xCF44, 0xFDDF, 0xEC56, 0x98E9, 0x8960, 0xBBFB, 0xAA72,
+  0x6306, 0x728F, 0x4014, 0x519D, 0x2522, 0x34AB, 0x0630, 0x17B9,
+  0xEF4E, 0xFEC7, 0xCC5C, 0xDDD5, 0xA96A, 0xB8E3, 0x8A78, 0x9BF1,
+  0x7387, 0x620E, 0x5095, 0x411C, 0x35A3, 0x242A, 0x16B1, 0x0738,
+  0xFFCF, 0xEE46, 0xDCDD, 0xCD54, 0xB9EB, 0xA862, 0x9AF9, 0x8B70,
+  0x8408, 0x9581, 0xA71A, 0xB693, 0xC22C, 0xD3A5, 0xE13E, 0xF0B7,
+  0x0840, 0x19C9, 0x2B52, 0x3ADB, 0x4E64, 0x5FED, 0x6D76, 0x7CFF,
+  0x9489, 0x8500, 0xB79B, 0xA612, 0xD2AD, 0xC324, 0xF1BF, 0xE036,
+  0x18C1, 0x0948, 0x3BD3, 0x2A5A, 0x5EE5, 0x4F6C, 0x7DF7, 0x6C7E,
+  0xA50A, 0xB483, 0x8618, 0x9791, 0xE32E, 0xF2A7, 0xC03C, 0xD1B5,
+  0x2942, 0x38CB, 0x0A50, 0x1BD9, 0x6F66, 0x7EEF, 0x4C74, 0x5DFD,
+  0xB58B, 0xA402, 0x9699, 0x8710, 0xF3AF, 0xE226, 0xD0BD, 0xC134,
+  0x39C3, 0x284A, 0x1AD1, 0x0B58, 0x7FE7, 0x6E6E, 0x5CF5, 0x4D7C,
+  0xC60C, 0xD785, 0xE51E, 0xF497, 0x8028, 0x91A1, 0xA33A, 0xB2B3,
+  0x4A44, 0x5BCD, 0x6956, 0x78DF, 0x0C60, 0x1DE9, 0x2F72, 0x3EFB,
+  0xD68D, 0xC704, 0xF59F, 0xE416, 0x90A9, 0x8120, 0xB3BB, 0xA232,
+  0x5AC5, 0x4B4C, 0x79D7, 0x685E, 0x1CE1, 0x0D68, 0x3FF3, 0x2E7A,
+  0xE70E, 0xF687, 0xC41C, 0xD595, 0xA12A, 0xB0A3, 0x8238, 0x93B1,
+  0x6B46, 0x7ACF, 0x4854, 0x59DD, 0x2D62, 0x3CEB, 0x0E70, 0x1FF9,
+  0xF78F, 0xE606, 0xD49D, 0xC514, 0xB1AB, 0xA022, 0x92B9, 0x8330,
+  0x7BC7, 0x6A4E, 0x58D5, 0x495C, 0x3DE3, 0x2C6A, 0x1EF1, 0x0F78,
 ]
 
 def dji_calculate_crc16_part(buf, pcrc):
-  """A non-standard crc16 hashing algorithm, looks like 32-bit one from Ambarella simply cut down in bits."""
-  crc = pcrc
-  for octet in buf:
-      crc = crc16_tab[(crc ^ octet) & 0xff] ^ (crc >> 8)
-  return crc & 0xffff
+    """ A non-standard crc16 hashing algorithm, looks like 32-bit one from Ambarella simply cut down in bits.
+    """
+    crc = pcrc
+    for octet in buf:
+        crc = crc16_tab[(crc ^ octet) & 0xff] ^ (crc >> 8)
+    return crc & 0xffff
 
 
 def dji_decrypt_block(cipher_buf, enc_key, enc_iv):
-  """ Decrypts a buffer with AES in a DJI way. """
-  block_sz = 256
-  plain_buf = b""
-  for cbpos in range(0, len(cipher_buf), block_sz):
-      # Reinit the crypto for each block, this is how Dji does it
-      crypto = AES.new(enc_key, AES.MODE_CBC, enc_iv)
-      plain_buf += crypto.decrypt(cipher_buf[cbpos:cbpos+block_sz])
-  return plain_buf, enc_iv
+    """ Decrypts a buffer with AES in a DJI way.
+    """
+    block_sz = 256
+    plain_buf = b""
+    for cbpos in range(0, len(cipher_buf), block_sz):
+        # Reinit the crypto for each block, this is how Dji does it
+        crypto = AES.new(enc_key, AES.MODE_CBC, enc_iv)
+        plain_buf += crypto.decrypt(cipher_buf[cbpos:cbpos+block_sz])
+    return plain_buf, enc_iv
 
 
 def dji_encrypt_block(cipher_buf, enc_key, enc_iv):
-  """ Encrypts a buffer with AES in a DJI way. """
-  block_sz = 256
-  plain_buf = b""
-  for cbpos in range(0, len(cipher_buf), block_sz):
-      # Reinit the crypto for each block, this is how Dji does it
-      crypto = AES.new(enc_key, AES.MODE_CBC, enc_iv)
-      plain_buf += crypto.encrypt(cipher_buf[cbpos:cbpos+block_sz])
-  return plain_buf, enc_iv
+    """ Encrypts a buffer with AES in a DJI way.
+    """
+    block_sz = 256
+    plain_buf = b""
+    for cbpos in range(0, len(cipher_buf), block_sz):
+        # Reinit the crypto for each block, this is how Dji does it
+        crypto = AES.new(enc_key, AES.MODE_CBC, enc_iv)
+        plain_buf += crypto.encrypt(cipher_buf[cbpos:cbpos+block_sz])
+    return plain_buf, enc_iv
 
 
 def dji_write_fwpkg_head(po, pkghead, minames):
-  fname = "{:s}_head.ini".format(po.mdprefix)
-  fwheadfile = open(fname, "w")
-  pkghead.ini_export(fwheadfile)
-  fwheadfile.write("{:s}={:s}\n".format("modules",' '.join(minames)))
-  fwheadfile.close()
+    fname = "{:s}_head.ini".format(po.mdprefix)
+    fwheadfile = open(fname, "w")
+    pkghead.ini_export(fwheadfile)
+    fwheadfile.write("{:s}={:s}\n".format("modules",' '.join(minames)))
+    fwheadfile.close()
 
 
 def dji_read_fwpkg_head(po):
-  pkghead = FwPkgHeader()
-  fname = "{:s}_head.ini".format(po.mdprefix)
-  parser = configparser.ConfigParser()
-  with open(fname, "r") as lines:
-    lines = itertools.chain(("[asection]",), lines)  # This line adds section header to ini
-    parser.read_file(lines)
-  # Set magic fields properly
-  pkgformat = parser.get("asection", "pkg_format").encode("utf-8")
-  pkghead.set_format_version(int(pkgformat))
-  if parser.has_option('asection', 'magic_ver'):
-      magicver_s = parser.get('asection', 'magic_ver')
-      pkghead.magic_ver = int(magicver_s,16)
-  # Set the rest of the fields
-  pkghead.manufacturer = parser.get("asection", "manufacturer").encode("utf-8")
-  pkghead.model = parser.get("asection", "model").encode("utf-8")
-  pkghead.timestamp = timegm(strptime(parser.get("asection", "timestamp"),"%Y-%m-%d %H:%M:%S"))
-  ver_latest_s = parser.get("asection", "ver_latest")
-  ver_latest_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9A-Fa-f]+)', ver_latest_s)
-  pkghead.set_ver_latest( ((int(ver_latest_m.group("major"),10)&0xff)<<24) + ((int(ver_latest_m.group("minor"),10)&0xff)<<16) + (int(ver_latest_m.group("svn"),10)&0xffff) )
-  ver_rollbk_s = parser.get("asection", "ver_rollbk")
-  ver_rollbk_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9A-Fa-f]+)', ver_rollbk_s)
-  pkghead.set_ver_rollbk( ((int(ver_rollbk_m.group("major"),10)&0xff)<<24) + ((int(ver_rollbk_m.group("minor"),10)&0xff)<<16) + (int(ver_rollbk_m.group("svn"),10)&0xffff) )
-  minames_s = parser.get("asection", "modules")
-  minames = minames_s.split(' ')
-  pkghead.entry_count = len(minames)
-  pkghead.hdrend_offs = sizeof(pkghead) + sizeof(FwPkgEntry)*pkghead.entry_count + sizeof(c_ushort)
-  del parser
-  return (pkghead, minames)
+    pkghead = FwPkgHeader()
+    fname = "{:s}_head.ini".format(po.mdprefix)
+    parser = configparser.ConfigParser()
+    with open(fname, "r") as lines:
+        lines = itertools.chain(("[asection]",), lines)  # This line adds section header to ini
+        parser.read_file(lines)
+    # Set magic fields properly
+    pkgformat = parser.get("asection", "pkg_format").encode("utf-8")
+    pkghead.set_format_version(int(pkgformat))
+    if parser.has_option('asection', 'magic_ver'):
+        magicver_s = parser.get('asection', 'magic_ver')
+        pkghead.magic_ver = int(magicver_s,16)
+    # Set the rest of the fields
+    pkghead.manufacturer = parser.get("asection", "manufacturer").encode("utf-8")
+    pkghead.model = parser.get("asection", "model").encode("utf-8")
+    pkghead.timestamp = timegm(strptime(parser.get("asection", "timestamp"),"%Y-%m-%d %H:%M:%S"))
+    ver_latest_s = parser.get("asection", "ver_latest")
+    ver_latest_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9A-Fa-f]+)', ver_latest_s)
+    pkghead.set_ver_latest(
+       ((int(ver_latest_m.group("major"), 10) & 0xff) << 24) +
+       ((int(ver_latest_m.group("minor"), 10) & 0xff) << 16) +
+       (int(ver_latest_m.group("svn"), 10) & 0xffff)
+    )
+    ver_rollbk_s = parser.get("asection", "ver_rollbk")
+    ver_rollbk_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9A-Fa-f]+)', ver_rollbk_s)
+    pkghead.set_ver_rollbk(
+      ((int(ver_rollbk_m.group("major"), 10) & 0xff) << 24) +
+      ((int(ver_rollbk_m.group("minor"), 10) & 0xff) << 16) +
+      (int(ver_rollbk_m.group("svn"), 10) & 0xffff)
+    )
+    minames_s = parser.get("asection", "modules")
+    minames = minames_s.split(' ')
+    pkghead.entry_count = len(minames)
+    pkghead.hdrend_offs = sizeof(pkghead) + sizeof(FwPkgEntry)*pkghead.entry_count + sizeof(c_ushort)
+    del parser
+    return (pkghead, minames)
 
 
 def dji_write_fwentry_head(po, i, e, miname):
-  fname = "{:s}_{:s}.ini".format(po.mdprefix,miname)
-  fwheadfile = open(fname, "w")
-  e.ini_export(fwheadfile)
-  fwheadfile.close()
+    fname = "{:s}_{:s}.ini".format(po.mdprefix,miname)
+    fwheadfile = open(fname, "w")
+    e.ini_export(fwheadfile)
+    fwheadfile.close()
 
 
 def dji_read_fwentry_head(po, i, miname):
-  hde = FwPkgEntry()
-  fname = "{:s}_{:s}.ini".format(po.mdprefix,miname)
-  parser = configparser.ConfigParser()
-  with open(fname, "r") as lines:
-    lines = itertools.chain(("[asection]",), lines)  # This line adds section header to ini
-    parser.read_file(lines)
-  target_s = parser.get("asection", "target")
-  target_m = re.search('m(?P<kind>[0-9]{2})(?P<model>[0-9]{2})', target_s)
-  hde.target = ((int(target_m.group("kind"),10)&0x1f)) + ((int(target_m.group("model"),10)&0x07)<<5)
-  version_s = parser.get("asection", "version")
-  version_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9]+)', version_s)
-  hde.version = ((int(version_m.group("major"),10)&0xff)<<24) + ((int(version_m.group("minor"),10)%0xff)<<16) + (int(version_m.group("svn"),10)%0xffff)
-  if parser.has_option("asection", "preencrypted"):
-    hde.preencrypted = int(parser.get("asection", "preencrypted"),10)
-  if (hde.preencrypted):
-    decrypted_md5_s = parser.get("asection", "decrypted_md5")
-    hde.decrypted_md5 = (c_ubyte * 16).from_buffer_copy(binascii.unhexlify(decrypted_md5_s))
-  hde.set_encrypt_type( int(parser.get("asection", "encrypt_type"),10) )
-  hde.set_splvalue( int(parser.get("asection", "splvalue"),10) )
-  hde.reserved2 = int(parser.get("asection", "reserved2"),16)
-  del parser
-  return (hde)
+    hde = FwPkgEntry()
+    fname = "{:s}_{:s}.ini".format(po.mdprefix,miname)
+    parser = configparser.ConfigParser()
+    with open(fname, "r") as lines:
+        lines = itertools.chain(("[asection]",), lines)  # This line adds section header to ini
+        parser.read_file(lines)
+    target_s = parser.get("asection", "target")
+    target_m = re.search('m(?P<kind>[0-9]{2})(?P<model>[0-9]{2})', target_s)
+    hde.target = ((int(target_m.group("kind"),10)&0x1f)) + ((int(target_m.group("model"),10)&0x07)<<5)
+    version_s = parser.get("asection", "version")
+    version_m = re.search('(?P<major>[0-9]+)[.](?P<minor>[0-9]+)[.](?P<svn>[0-9]+)', version_s)
+    hde.version = (
+      ((int(version_m.group("major"), 10) & 0xff) << 24) +
+      ((int(version_m.group("minor"), 10) % 0xff) << 16) +
+      (int(version_m.group("svn"), 10) % 0xffff)
+    )
+    if parser.has_option("asection", "preencrypted"):
+        hde.preencrypted = int(parser.get("asection", "preencrypted"),10)
+    if (hde.preencrypted):
+        decrypted_md5_s = parser.get("asection", "decrypted_md5")
+        hde.decrypted_md5 = (c_ubyte * 16).from_buffer_copy(binascii.unhexlify(decrypted_md5_s))
+    hde.set_encrypt_type( int(parser.get("asection", "encrypt_type"),10) )
+    hde.set_splvalue( int(parser.get("asection", "splvalue"),10) )
+    hde.reserved2 = int(parser.get("asection", "reserved2"),16)
+    del parser
+    return (hde)
 
 def dji_extract(po, fwpkgfile):
   pkghead = FwPkgHeader()
@@ -469,7 +500,8 @@ def dji_extract(po, fwpkgfile):
           print(hde)
       curhead_checksum = dji_calculate_crc16_part((c_ubyte * sizeof(hde)).from_buffer_copy(hde), curhead_checksum)
       if hde.stored_len != hde.decrypted_len:
-          eprint("{}: Warning: decrypted size differs from stored one, {:d} instead of {:d}; this is not supported.".format(po.fwpkg,hde.decrypted_len,hde.stored_len))
+          eprint("{}: Warning: decrypted size differs from stored one, {:d} instead of {:d}; this is not supported."
+            .format(po.fwpkg,hde.decrypted_len,hde.stored_len))
       chksum_enctype = hde.get_encrypt_type()
       if (chksum_enctype != 0):
           if (po.no_crypto):
@@ -488,12 +520,14 @@ def dji_extract(po, fwpkgfile):
       raise EOFError("Couldn't read firmware package file header checksum.")
 
   if curhead_checksum != pkghead_checksum.value:
-      eprint("{}: Warning: Firmware package file header checksum did not match; should be {:04X}, found {:04X}.".format(po.fwpkg, pkghead_checksum.value, curhead_checksum))
+      eprint("{}: Warning: Firmware package file header checksum did not match; should be {:04X}, found {:04X}."
+        .format(po.fwpkg, pkghead_checksum.value, curhead_checksum))
   elif (po.verbose > 1):
       print("{}: Headers checksum {:04X} matches.".format(po.fwpkg,pkghead_checksum.value))
 
   if fwpkgfile.tell() != pkghead.hdrend_offs:
-      eprint("{}: Warning: Header end offset does not match; should end at {}, ends at {}.".format(po.fwpkg,pkghead.hdrend_offs,fwpkgfile.tell()))
+      eprint("{}: Warning: Header end offset does not match; should end at {}, ends at {}."
+        .format(po.fwpkg,pkghead.hdrend_offs,fwpkgfile.tell()))
 
   # Prepare array of names; "0" will mean empty index
   minames = ["0"]*len(pkgmodules)
@@ -549,12 +583,16 @@ def dji_extract(po, fwpkgfile):
           decrypted_chksum.update(copy_buffer)
       fwitmfile.close()
       if (stored_chksum.hexdigest() != hde.hex_stored_md5()):
-          eprint("{}: Warning: Module index {:d} stored checksum mismatch; got {:s}, expected {:s}.".format(po.fwpkg,i,stored_chksum.hexdigest(),hde.hex_stored_md5()))
+          eprint("{}: Warning: Module index {:d} stored checksum mismatch; got {:s}, expected {:s}."
+            .format(po.fwpkg,i,stored_chksum.hexdigest(),hde.hex_stored_md5()))
       if (not hde.preencrypted) and (decrypted_chksum.hexdigest() != hde.hex_decrypted_md5()):
-          eprint("{}: Warning: Module index {:d} decrypted checksum mismatch; got {:s}, expected {:s}.".format(po.fwpkg,i,decrypted_chksum.hexdigest(),hde.hex_decrypted_md5()))
-          eprint("{}: Module index {:d} may be damaged due to bad decryption; use no-crypto option to leave it as-is.".format(po.fwpkg,i))
+          eprint("{}: Warning: Module index {:d} decrypted checksum mismatch; got {:s}, expected {:s}."
+            .format(po.fwpkg,i,decrypted_chksum.hexdigest(),hde.hex_decrypted_md5()))
+          eprint("{}: Module index {:d} may be damaged due to bad decryption; use no-crypto option to leave it as-is."
+            .format(po.fwpkg,i))
       if (not hde.preencrypted) and (decrypted_n != hde.decrypted_len):
-          eprint("{}: Warning: decrypted size mismatch, {:d} instead of {:d}.".format(po.fwpkg,decrypted_n,hde.decrypted_len))
+          eprint("{}: Warning: decrypted size mismatch, {:d} instead of {:d}."
+            .format(po.fwpkg,decrypted_n,hde.decrypted_len))
       if (po.verbose > 1):
           print("{}: Module index {:d} stored checksum {:s}".format(po.fwpkg,i,stored_chksum.hexdigest()))
 
@@ -600,9 +638,11 @@ def dji_create(po, fwpkgfile):
       if (chksum_enctype != 0) and (not hde.preencrypted):
           if (po.no_crypto):
               if (not po.force_continue):
-                  eprint("{}: Error: Module {:d} needs encryption {:d}, but crypto is disabled.".format(po.fwpkg,chksum_enctype,i))
+                  eprint("{}: Error: Module {:d} needs encryption {:d}, but crypto is disabled."
+                    .format(po.fwpkg,chksum_enctype,i))
                   exit(1)
-              eprint("{}: Warning: Module {:d} needs encryption {:d}, but crypto is disabled; switching to unencrypted.".format(po.fwpkg,chksum_enctype,i))
+              eprint("{}: Warning: Module {:d} needs encryption {:d}, but crypto is disabled; switching to unencrypted."
+                .format(po.fwpkg,chksum_enctype,i))
               hde.set_encrypt_type(0)
               chksum_enctype = hde.get_encrypt_type()
           elif (chksum_enctype == 1):
